a2a5e7cfe4736a020634ebefaca0ebbd146254a8
Botez Alexandru <alexandru.botez@xivic.com>
2016-08-29 17:46:28 +0300

    responsive

--- js/book-slider.js
+++ js/book-slider.js
@@ -1,6 +1,13 @@
 $(document).ready(function () {
   $('.book-slider').slick({
     slidesToShow: 2,
-    slidesToScroll: 1
+    slidesToScroll: 1,
+    responsive: [{
+      breakpoint: 380,
+      settings: {
+        slidesToShow: 1,
+        arrows:false
+      }
+    }]
   });
 });
