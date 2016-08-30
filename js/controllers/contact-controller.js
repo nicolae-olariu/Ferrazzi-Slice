417cd2fda37793ed0a330f36a169a04a4051c6bd
Botez Alexandru <alexandru.botez@xivic.com>
2016-08-30 17:35:23 +0300

    addded label for marker

--- js/controllers/contact-controller.js
+++ js/controllers/contact-controller.js
@@ -4,23 +4,27 @@
   angular.module('contact-module')
     .controller('contactController', contactController);
 
-  function contactController($timeout ) {
+  function contactController($timeout, uiGmapIsReady) {
 
     var vm = this;
     vm.map = {
       center: {
-        latitude: 34.042575,
-        longitude: -118.445391
+        latitude: 34.042444,
+        longitude: -118.445368
       },
       zoom: 18,
       marker: {
-        idKey: 1
+        idKey: 1,
+        options: {
+          labelContent: 'ferrazzi greenlight',
+          labelAnchor: "-10 12",
+          labelClass: 'labelClass',
+          labelInBackground: true
+        }
       },
       window: {
         show: true
       }
     };
-
-
   }
 })();
