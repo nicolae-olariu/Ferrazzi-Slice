327a589a1c3af79d821a0ada10240ed023d7507b
Botez Alexandru <alexandru.botez@xivic.com>
2016-08-31 12:07:59 +0300

    map is disabled

--- js/controllers/contact-controller.js
+++ js/controllers/contact-controller.js
@@ -7,12 +7,18 @@
   function contactController() {
 
     var vm = this;
+
+    //verify width for disabling the map drag on mobile
+    var widthForMap = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+    var isDraggable = widthForMap > 767;
+
     vm.map = {
       center: {
         latitude: 34.042444,
         longitude: -118.445368
       },
       options: {
+        draggable : isDraggable,
         disableDefaultUI: true
       },
       zoom: 18,
