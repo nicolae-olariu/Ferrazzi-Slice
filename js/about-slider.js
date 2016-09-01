7a42da0829db363eead8c18992a5742c56d4ec44
Botez Alexandru <alexandru.botez@xivic.com>
2016-09-01 17:49:05 +0300

    slider mobile

--- js/about-slider.js
+++ js/about-slider.js
@@ -4,6 +4,23 @@ $(document).ready(function(){
     nav: true,
     navText : ["",""],
     dots: false,
-    margin: 12
+    margin: 12,
+    responsive:{
+      0:{
+        items:1,
+        nav:false,
+        autoplay:true,
+        autoplayTimeout: 3000,
+        autoplaySpeed: 1000,
+        loop: true
+
+      },
+      700:{
+        items:2
+      },
+      1000:{
+        items:3
+      }
+    }
   });
 });
