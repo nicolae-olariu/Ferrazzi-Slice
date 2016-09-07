4d3a03801adba79e431e28015800c06f59957f62
Botez Alexandru <alexandru.botez@xivic.com>
2016-09-07 17:15:43 +0300

    jquery validator

--- js/xivic.js
+++ js/xivic.js
@@ -1,7 +1,7 @@
-$( document ).ready(function() {
+$(document).ready(function () {
   "use strict";
 
-  var applyButton, applyForm, htmlBody, formId, uploadCv, textCv, contactForm;
+  var applyButton, applyForm, htmlBody, formId, uploadCv, textCv, contactForm, emailInput;
 
   applyButton = $('.apply-button');
   applyForm = $('.position-section__form');
@@ -10,38 +10,96 @@ $( document ).ready(function() {
   uploadCv = $('.upload-cv');
   textCv = $('.cv-name > p');
   contactForm = $('form[name="contactForm"]');
+  emailInput = $('input[name="email"]');
 
-  applyButton.on('click', function() {
+  applyButton.on('click', function () {
     applyForm.addClass('form__is-expanded');
-    htmlBody.animate({scrollTop: formId.offset().top},'slow');
+    htmlBody.animate({scrollTop: formId.offset().top}, 'slow');
     applyButton.addClass('hidden');
   });
 
-  uploadCv.on('change', function(e) {
+  uploadCv.on('change', function (e) {
     var fileName = e.target.files[0];
     //if there is no file change it
-    fileName = fileName ? fileName.name:'';
+    fileName = fileName ? fileName.name : '';
     // name of the file in html
     textCv.text(fileName);
   });
 
-  contactForm.on("submit", function (event) {
-    event.preventDefault();
-    var formData = new FormData(this);
-    $.ajax({
-      url: 'php/upload-form.php',
-      type: 'POST',
-      data: formData,
-      async: false,
-      success: function (data) {
-        alert(data)
+  function validateEmail(email) {
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+  }
+  contactForm.validate({
+    debug: true,
+    ignore: [],
+    rules: {
+      name: 'required',
+      phone: {
+        required: true,
+        digits: true
       },
-      cache: false,
-      contentType: false,
-      processData: false
-    });
+      email: {
+        required: true,
+        email: true
+      },
+      cv: {
+        required: true
+      }
+    },
+    messages: {
+      name: 'Please provide your name',
+      phone: {
+        required:'Please provide your number',
+        digits: 'please provide a valid number'
+      },
+      email: {
+        required: 'please enter an email address',
+        email:'Please enter a valid email address'
+      },
+      cv: 'Please upload your cv'
+    },
+    invalidHandler: function (form) {
+      debugger;
 
-    return false;
-  })
+    },
+    submitHandler: function (form) {
+      var formData = new FormData(form);
+        $.ajax({
+          url: 'php/upload-form.php',
+          type: 'POST',
+          data: formData,
+          async: false,
+          success: function (data) {
+            alert(data)
+          },
+          cache: false,
+          contentType: false,
+          processData: false
+        });
+    }
+  });
+
+  // contactForm.on("submit", function (event) {
+  //   event.preventDefault();
+  //
+  //   debugger;
+  //
+  //   // var formData = new FormData(this);
+  //   // $.ajax({
+  //   //   url: 'php/upload-form.php',
+  //   //   type: 'POST',
+  //   //   data: formData,
+  //   //   async: false,
+  //   //   success: function (data) {
+  //   //     alert(data)
+  //   //   },
+  //   //   cache: false,
+  //   //   contentType: false,
+  //   //   processData: false
+  //   // });
+  //
+  //   return false;
+  // })
 
 });
